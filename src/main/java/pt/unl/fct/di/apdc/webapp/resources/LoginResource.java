bd88781ed30e34207a3d00e8a5f24d0b5fb91928
gpeduarte <gpe.duarte@campus.fct.unl.pt>
2023-04-05 15:47:37 +0100

    Added Login and Register Operations

---
+++ src/main/java/pt/unl/fct/di/apdc/webapp/resources/LoginResource.java
@@ -0,0 +1,135 @@
+package pt.unl.fct.di.apdc.webapp.resources;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.ws.rs.POST;
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.PathElement;
+import com.google.cloud.datastore.StringValue;
+import com.google.cloud.datastore.Transaction;
+import com.google.gson.Gson;
+
+import pt.unl.fct.di.apdc.webapp.util.AuthToken;
+import pt.unl.fct.di.apdc.webapp.util.LoginData;
+
+@Path("/login")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class LoginResource {
+
+	private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+	
+	private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+	
+	private KeyFactory userKeyFactory = datastore.newKeyFactory().setKind("User");
+
+	private final Gson g = new Gson();
+	
+	public LoginResource() {}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Response doLogin(LoginData data,
+			@Context HttpServletRequest request,
+			@Context HttpHeaders headers) {
+		LOG.fine("Login attempt by user: " + data.username);
+		
+		
+		Key userKey = userKeyFactory.newKey(data.username);
+		Key ctrsKey = datastore.newKeyFactory()
+				.addAncestors(PathElement.of("User", data.username))
+				.setKind("UserStats").newKey("counters");
+		Key logKey = datastore.allocateId(
+				datastore.newKeyFactory()
+				.addAncestors(PathElement.of("User", data.username))
+				.setKind("UserLog").newKey()
+				);
+		Transaction txn = datastore.newTransaction();
+		try {
+			Entity user = txn.get(userKey);
+			if(user == null) {
+				LOG.warning("Failed login attempt for username: " + data.username);
+				return Response.status(Status.FORBIDDEN).build();
+			}
+			
+			Entity stats = txn.get(ctrsKey);
+			if(stats == null) {
+				stats = Entity.newBuilder(ctrsKey)
+						.set("user_stats_logins", 0L)
+						.set("user_stats_failed", 0L)
+						.set("user_first_login", Timestamp.now())
+						.set("user_last_login", Timestamp.now())
+						.build();
+			}
+			
+			String hashedPWD = (String) user.getString("user_pwd");
+			if(hashedPWD.equals(DigestUtils.sha3_512Hex(data.password))) {
+				Entity log = Entity.newBuilder(logKey)
+						.set("user_login_ip", request.getRemoteAddr())
+						.set("user_login_host", request.getRemoteHost())
+						.set("user_login_latlon",
+								StringValue.newBuilder(headers.getHeaderString("X-AppEngine-CityLatLong"))
+										.setExcludeFromIndexes(true).build())
+						.set("user_login_city", headers.getHeaderString("X-AppEngine-City"))
+						.set("user_login_country", headers.getHeaderString("X-AppEngine-Country"))
+						.set("user_login_time", Timestamp.now())
+						.build();
+				Entity ustats = Entity.newBuilder(ctrsKey)
+						.set("user_stats_logins", 1L + stats.getLong("user_stats_logins"))
+						.set("user_stats_failed", 0L)
+						.set("user_first_login", stats.getTimestamp("user_first_login"))
+						.set("user_last_login", Timestamp.now())
+						.build();
+				txn.put(log, ustats);
+				txn.commit();
+				
+				AuthToken token = new AuthToken(data.username);
+				LOG.info("User '" + data.username + "' logged in sucessfully");
+				return Response.ok(g.toJson(token)).build();
+			} else {
+				Entity ustats = Entity.newBuilder(ctrsKey)
+						.set("user_stats_logins", stats.getLong("user_stats_logins"))
+						.set("user_stats_failed", 1L + stats.getLong("user_stats_failed"))
+						.set("user_first_login", stats.getTimestamp("user_first_login"))
+						.set("user_last_login", stats.getTimestamp("user_last_login"))
+						.set("user_kast_attempt", Timestamp.now())
+						.build();
+				txn.put(ustats);
+				txn.commit();
+				LOG.warning("Wrong password for user: " + data.username);
+				return Response.status(Status.FORBIDDEN).build();
+			}
+		} catch (Exception e) {
+			txn.rollback();
+			LOG.severe(e.getMessage());
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+			if(txn.isActive()) {
+				txn.rollback();
+				return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+			}
+		}
+	}
+
+}
